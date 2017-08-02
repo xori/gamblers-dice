f68dbd31a14137a6585f7ba7213b47998a5c6f7f
Plusb Preco <plusb21@gmail.com>
2017-08-02 14:56:35 +0900

    Cleanup `index.js`

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-
 class Die {
-  constructor(size) {
-    size = size || 6;
+  constructor(size = 6) {
     this.state = new Array(size).fill(1);
   }
 
@@ -13,17 +11,17 @@ class Die {
 
     // find the target we hit
     let target = 0;
-    while (steps >= 0) { 
-      steps -= this.state[target++]; 
+    while (steps >= 0) {
+      steps -= this.state[target++];
     }
 
-    // at this point, the target is what we want to return, 
+    // at this point, the target is what we want to return,
     // so use target-1 to update the zero-based states
     this.state = this.state.map((prev_state, index) =>
-      (index === target - 1) ? 1 : prev_state + 1 );
+      (index === target - 1) ? 1 : prev_state + 1);
 
     return target;
   }
 }
 
-module.exports = Die
+module.exports = Die;
