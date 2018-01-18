c5fed96dc40f701df1801d5ff848584d0c1cd6f5
Plusb Preco <plusb21@gmail.com>
2018-01-19 01:09:39 +0900

    Cleanup `index.js` (#13)

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-
 class Die {
-  constructor(size) {
-    size = size || 6;
+  constructor(size = 6) {
     this.state = new Array(size).fill(1);
   }
 
@@ -9,21 +7,21 @@ class Die {
     // add all of the states up
     const sum = this.state.reduce((p, c) => p + c, 0);
     // get a random number of steps between 0 and the sum
-    let steps =  sum * Math.random();
+    let steps = sum * Math.random();
 
     // find the target we hit
     let target = 0;
-    while (steps >= 0) { 
-      steps -= this.state[target++]; 
+    while (steps >= 0) {
+      steps -= this.state[target++];
     }
 
-    // at this point, the target is what we want to return, 
+    // at this point, the target is what we want to return,
     // so use target-1 to update the zero-based states
     this.state = this.state.map((prev_state, index) =>
-      (index === target - 1) ? 1 : prev_state + 1 );
+      (index === target - 1) ? 1 : prev_state + 1);
 
     return target;
   }
 }
 
-module.exports = Die
+module.exports = Die;
